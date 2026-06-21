047dcc2c611e07457252c5dface312fc03fb4302
ayouberrak2 <ayouberrak318@gmail.com>
2026-06-21 22:28:04 +0100

    chore: remove legacy dependency probe

--- phase-01-python-data/src/main.py
+++
@@ -1,9 +0,0 @@
-import matplotlib as mat
-import numpy as num
-import pandas
-import psycopg
-
-print(num.__version__ +"");
-print(mat.__version__);
-print(pandas.__version__);
-print(psycopg.__version__);
